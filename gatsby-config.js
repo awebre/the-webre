94844b74ffc7399a13e69b81deaf7fcfae5b44d7
Austin Webre <austin.webre@gmail.com>
2020-09-10 19:32:31 -0500

    added some themes? I have no idea how this stuff works

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,29 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "theWebre",
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/austinwebre`,
+      },
+      {
+        name: `Github`,
+        url: `https://github.com/awebre`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-theme-material-ui`,
+    },
+  ],
 }
